43e1d80cc8808c29f00c624476acb0fdbee3a229
unknown <sara5aguilo@gmail.com>
2023-06-25 20:54:24 +0200

    Initial commit

---
+++ src/main/java/com/bootstrap/HelloApplication.java
@@ -0,0 +1,16 @@
+package com.bootstrap;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
+public class HelloApplication {
+    public static void main(String[] args) {
+
+    }
+}
